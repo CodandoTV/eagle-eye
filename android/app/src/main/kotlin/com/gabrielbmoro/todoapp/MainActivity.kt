9e100664c8ff7b71fba4d24ac07a4f21ac9a70c5
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-08-30 18:26:42 -0300

    Remove unnecessary files

--- android/app/src/main/kotlin/com/gabrielbmoro/todoapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.gabrielbmoro.todoapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:todoapp/ui/todo_app.dart';
-
-void main() {
-  runApp(TodoApp());
-}
